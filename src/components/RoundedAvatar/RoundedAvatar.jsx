73cb7d0aeac0b03243962739c2d7e08c0fc98a63
Хайрутдинов Нияз Назымович <251920@mck-ktits.ru>
2023-01-16 18:55:13 +0300

    new commit

---
+++ src/components/RoundedAvatar/RoundedAvatar.jsx
@@ -0,0 +1,10 @@
+const RoundedAvatar = (props) => {
+
+    return (
+        <div>
+            <img src={props.url}
+            alt="Avatar" />
+        </div>
+    )
+}
+export default RoundedAvatar;
